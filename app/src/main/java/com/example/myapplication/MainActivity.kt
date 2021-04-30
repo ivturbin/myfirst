106389600a0f0ab6e7de8a8ab51b3ee409a94fba
Ivan Turbin <van_turbin@mail.ru>
2021-04-30 21:26:39 +0400

    Промежуточный

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val practice: Practice = Practice()
+        val practice = Practice()
         practice.secondTask()
         practice.thirdTask()
         practice.fourthTask()
     }
 }
+
